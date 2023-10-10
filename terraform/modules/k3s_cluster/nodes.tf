8fb1c91791b8c27fbe0f86db323fb5f855f1fac0
Matthew Vance <mavance44@gmail.com>
2023-10-09 21:49:09 -0400

    automate k3s cluster configuration

--- terraform/modules/k3s_cluster/nodes.tf
+++ terraform/modules/k3s_cluster/nodes.tf
@@ -1,4 +1,64 @@
-resource "proxmox_vm_qemu" "k8s_manager" {
+resource "proxmox_vm_qemu" "k3s_clb" {
+  count       = 2
+  name        = "k3s-clb-${count.index}"
+  desc        = "k3s cluster load balancer"
+  tags        = "k3s;lb"
+  onboot      = true
+  target_node = var.proxmox_host
+  clone       = var.template_name
+  os_type     = "cloud-init"
+  qemu_os     = "l26"
+  cpu         = "kvm64"
+  cores       = 2
+  sockets     = 1
+  memory      = 4096
+  scsihw      = "virtio-scsi-pci"
+  disk {
+    size    = "10G"
+    type    = "scsi"
+    storage = "local-zfs"
+  }
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  ipconfig0 = "ip=10.10.3.${count.index + 90}/24,gw=10.10.3.1"
+  sshkeys   = var.ssh_key
+}
+
+resource "proxmox_vm_qemu" "k3s_alb" {
+  count       = 2
+  name        = "k3s-alb-${count.index}"
+  desc        = "k3s application load balancer"
+  tags        = "k3s;lb"
+  onboot      = true
+  target_node = var.proxmox_host
+  clone       = var.template_name
+  os_type     = "cloud-init"
+  qemu_os     = "l26"
+  cpu         = "kvm64"
+  cores       = 2
+  sockets     = 1
+  memory      = 4086
+  scsihw      = "virtio-scsi-pci"
+  disk {
+    size    = "10G"
+    type    = "scsi"
+    storage = "local-zfs"
+  }
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  ipconfig0 = "ip=10.10.3.${count.index + 95}/24,gw=10.10.3.1"
+  sshkeys   = var.ssh_key
+}
+
+resource "proxmox_vm_qemu" "k3s_server" {
   count       = var.server_count
   name        = "k3s-server-${count.index}"
   desc        = "k3s server node"
@@ -28,7 +88,7 @@ resource "proxmox_vm_qemu" "k8s_manager" {
   sshkeys   = var.ssh_key
 }
 
-resource "proxmox_vm_qemu" "k8s_worker" {
+resource "proxmox_vm_qemu" "k3s_agent" {
   count       = var.agent_count
   name        = "k3s-agent-${count.index}"
   desc        = "k3s agent node"
